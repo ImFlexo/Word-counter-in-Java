cd638e10d12339b72098998a0cc8fb7d6e75c9d2
ImFlexo <97156492+ImFlexo@users.noreply.github.com>
2022-12-25 13:40:06 +0300

    Add files via upload

---
+++ src/word/counter/Main.java
@@ -0,0 +1,40 @@
+
+package word.counter;
+
+import java.util.Scanner;
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter your paragraph: ");
+        String userInput = sc.nextLine();
+        sc.close();
+        
+        userInput = userInput.trim();
+        
+        int wordCount = 0;
+        
+        if(userInput.length() == 0){
+        wordCount = 0;
+        
+        }
+        else{
+        wordCount++;
+        
+        for(int i = 0; i < userInput.length(); i++){
+                if(userInput.charAt(i) == ' ' && userInput.charAt(i + 1) != ' ') {
+                
+                wordCount++;
+                }
+        
+        }
+        
+        }
+        System.out.println("Word count: " + wordCount);
+    }
+    
+}
